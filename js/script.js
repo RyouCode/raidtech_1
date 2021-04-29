1c48ec52af45793192bf7f803c729f44e66cc766
RyouCode <81622428+RyouCode@users.noreply.github.com>
2021-04-30 01:41:45 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(function() {
+    window.addEventListener("scroll", function() {
+        var header = document.querySelector(".header");
+        header.classList.toggle("scroll-nav", window.scrollY > 0);
+    });
+});
